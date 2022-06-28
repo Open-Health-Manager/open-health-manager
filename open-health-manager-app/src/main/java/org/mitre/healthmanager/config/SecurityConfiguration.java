47c2a5f8e2577ce658b43135141b09067b621037
Karl Naden <knaden@mitre.org>
2022-06-27 23:07:51 -0400

    FHIR endpoints require auth, except metadata First stab at authorization interceptor

--- open-health-manager-app/src/main/java/org/mitre/healthmanager/config/SecurityConfiguration.java
+++ open-health-manager-app/src/main/java/org/mitre/healthmanager/config/SecurityConfiguration.java
@@ -95,6 +95,8 @@ public class SecurityConfiguration {
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/admin/**").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/api/**").authenticated()
+            .antMatchers("/fhir/metadata").permitAll()
+            .antMatchers("/fhir/**").authenticated()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/health/**").permitAll()
             .antMatchers("/management/info").permitAll()
