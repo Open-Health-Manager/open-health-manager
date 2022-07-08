98803d91b1741c6d3ee310815da77dc650add10a
Karl Naden <knaden@mitre.org>
2022-07-08 10:01:59 -0400

    Revert "FHIR endpoints require auth, except metadata"
    This reverts commit 98cf2b35e2edad4d128f6d295373dae28038088f.
    moving these changes to a different branch

--- open-health-manager-app/src/main/java/org/mitre/healthmanager/config/SecurityConfiguration.java
+++ open-health-manager-app/src/main/java/org/mitre/healthmanager/config/SecurityConfiguration.java
@@ -95,8 +95,6 @@ public class SecurityConfiguration {
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/admin/**").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/api/**").authenticated()
-            .antMatchers("/fhir/metadata").permitAll()
-            .antMatchers("/fhir/**").authenticated()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/health/**").permitAll()
             .antMatchers("/management/info").permitAll()
