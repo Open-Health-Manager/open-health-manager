79fa843586326ac887dd8cbc41b3dd6a934b1d19
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2022-09-07 19:01:40 -0400

    addd dstu2 converter

--- open-health-manager-lib/src/main/java/org/mitre/healthmanager/lib/pdr/data/AppleHealthKitService.java
+++ open-health-manager-lib/src/main/java/org/mitre/healthmanager/lib/pdr/data/AppleHealthKitService.java
@@ -4,8 +4,12 @@ import java.util.Base64;
 
 import javax.validation.constraints.NotNull;
 
+import org.hl7.fhir.convertors.factory.VersionConvertorFactory_10_40;
 import org.hl7.fhir.r4.model.AllergyIntolerance;
 import org.hl7.fhir.r4.model.Binary;
+import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
+import org.hl7.fhir.r4.model.CodeableConcept;
+import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.Condition;
 import org.hl7.fhir.r4.model.DateType;
 import org.hl7.fhir.r4.model.IdType;
@@ -15,9 +19,6 @@ import org.hl7.fhir.r4.model.Period;
 import org.hl7.fhir.r4.model.Procedure;
 import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.Resource;
-import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
-import org.hl7.fhir.r4.model.CodeableConcept;
-import org.hl7.fhir.r4.model.Coding;
 import org.springframework.integration.annotation.Transformer;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
@@ -28,34 +29,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gargoylesoftware.htmlunit.util.MimeType;
 import com.mysql.cj.util.StringUtils;
 
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.api.Constants;
 import ca.uhn.fhir.rest.server.exceptions.UnprocessableEntityException;
 
 @Service
 public class AppleHealthKitService extends DataTransformer {
 	public static final Coding PREGNANCY_CODING = new Coding().setCode("77386006").setSystem("http://snomed.info/sct");
 	
+	private static final FhirContext fhirContextforDstu2Hl7Org = FhirContext.forDstu2Hl7Org();
+	
 	@Transformer(inputChannel = "healthKitChannel")
-	BundleEntryComponent transform(BundleEntryComponent entry, @Header("internalPatientId") @NotNull String internalPatientId) {
+	public BundleEntryComponent transform(BundleEntryComponent entry, @Header("internalPatientId") @NotNull String internalPatientId) {
 		Resource resource = entry.getResource();
-        if (resource instanceof Observation) {
-        	((Observation)resource).getSubject().setReference("Patient/" + internalPatientId);
-           ((Observation)resource).setEncounter((Reference)null);
-        } else if (resource instanceof Procedure) {
-        	((Procedure)resource).getSubject().setReference("Patient/" + internalPatientId);
-           ((Procedure)resource).setEncounter((Reference)null);
-           ((Procedure)resource).getPerformer().clear();
-        } else if (resource instanceof Condition) {
-        	((Condition)resource).getSubject().setReference("Patient/" + internalPatientId);
-           ((Condition)resource).setAsserter((Reference)null);
-        } else if (resource instanceof AllergyIntolerance) {
-        	((AllergyIntolerance)resource).getPatient().setReference("Patient/" + internalPatientId);
-        } else if (resource instanceof Immunization) {
-        	((Immunization)resource).getPatient().setReference("Patient/" + internalPatientId);
-           ((Immunization)resource).setEncounter((Reference)null);
-           ((Immunization)resource).getPerformer().clear();
-        } else if (resource instanceof Binary) {
+        if (resource instanceof Binary) {
         	resource = convertBinary((Binary)resource, internalPatientId);
         }
+        fixReferences(resource, internalPatientId);
         entry.setResource(resource);
         return entry;
 	}
@@ -73,20 +63,21 @@ public class AppleHealthKitService extends DataTransformer {
 			}
 			String sampleType = node.get("sampleType").asText();
 			if(sampleType.equals("HKCategoryTypeIdentifierPregnancy")){
-				return convertPregnancy(node, internalPatientId);
+				return convertPregnancy(node);
 			}
+		} else if(binary.getContentType().equals(Constants.CT_FHIR_JSON_NEW)){
+			return convertDstu2(binary);
 		}
 		throw new UnprocessableEntityException("Unprocessable binary resource content type.");	
 	}
 	
-	private Condition convertPregnancy(JsonNode node, String internalPatientId) {
+	private Condition convertPregnancy(JsonNode node) {
 		String uuid = node.get("uuid").asText();
 		String startDateString = node.get("startDate").asText();
 		String endDateString = node.get("endDate").asText();
 		
 		Condition condition = new Condition();
 		condition.setId(new IdType("HKCategoryTypeIdentifierPregnancy", uuid));
-		condition.getSubject().setReference("Patient/" + internalPatientId);
 		condition.setCode(new CodeableConcept()
 				.addCoding(PREGNANCY_CODING));
 		
@@ -103,5 +94,37 @@ public class AppleHealthKitService extends DataTransformer {
 		}
 			
 		return condition;
+	}
+	
+	private Resource convertDstu2(Binary binary) {				
+		String jsonStringBase64 = binary.getDataElement().getValueAsString();
+		String jsonString = new String(Base64.getDecoder().decode(jsonStringBase64));
+		org.hl7.fhir.dstu2.model.Resource input = 
+				(org.hl7.fhir.dstu2.model.Resource) fhirContextforDstu2Hl7Org.newJsonParser().parseResource(jsonString);
+		
+		if(VersionConvertorFactory_10_40.convertsResource(input.getResourceType().name())) {
+			return VersionConvertorFactory_10_40.convertResource(input);
+		}
+		throw new UnprocessableEntityException("Unprocessable binary resource fhir type: " + input.getResourceType().name());			
+	}
+	
+	private void fixReferences(Resource resource, String internalPatientId) {
+        if (resource instanceof Observation) {
+        	((Observation)resource).getSubject().setReference("Patient/" + internalPatientId);
+           ((Observation)resource).setEncounter((Reference)null);
+        } else if (resource instanceof Procedure) {
+        	((Procedure)resource).getSubject().setReference("Patient/" + internalPatientId);
+           ((Procedure)resource).setEncounter((Reference)null);
+           ((Procedure)resource).getPerformer().clear();
+        } else if (resource instanceof Condition) {
+        	((Condition)resource).getSubject().setReference("Patient/" + internalPatientId);
+           ((Condition)resource).setAsserter((Reference)null);
+        } else if (resource instanceof AllergyIntolerance) {
+        	((AllergyIntolerance)resource).getPatient().setReference("Patient/" + internalPatientId);
+        } else if (resource instanceof Immunization) {
+        	((Immunization)resource).getPatient().setReference("Patient/" + internalPatientId);
+           ((Immunization)resource).setEncounter((Reference)null);
+           ((Immunization)resource).getPerformer().clear();
+        }
 	}
 }
