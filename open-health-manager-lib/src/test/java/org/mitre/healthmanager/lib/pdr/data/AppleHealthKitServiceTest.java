18cccb893fa2823b0169a3675fd3d00a22ad5803
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2022-10-28 12:00:16 -0400

    add tests

--- open-health-manager-lib/src/test/java/org/mitre/healthmanager/lib/pdr/data/AppleHealthKitServiceTest.java
+++ open-health-manager-lib/src/test/java/org/mitre/healthmanager/lib/pdr/data/AppleHealthKitServiceTest.java
@@ -6,8 +6,11 @@ import java.util.Base64;
 import org.hl7.fhir.r4.model.Binary;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Condition;
+import org.hl7.fhir.r4.model.Observation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import com.gargoylesoftware.htmlunit.util.MimeType;
 
 import ca.uhn.fhir.rest.api.Constants;
 
@@ -32,5 +35,24 @@ class AppleHealthKitServiceTest {
 		Condition condition = (Condition) result.getResource();
 		Assertions.assertEquals(condition.getSubject().getReferenceElement().getIdPart(), "user-2");
 		Assertions.assertNull(condition.getAsserter().getReferenceElement().getValue());
+	}
+	
+	@Test
+	void testTransformBloodPressureToR4() throws IOException {
+		byte[] bytes = this.getClass().getResourceAsStream("HKCorrelationTypeIdentifierBloodPressure.json").readAllBytes();
+		
+		Binary binary = new Binary();
+		binary.setContentType(MimeType.APPLICATION_JSON);
+		binary.setContentAsBase64(Base64.getEncoder().encodeToString(bytes));
+		
+		Bundle.BundleEntryComponent entry = new Bundle.BundleEntryComponent();
+		entry.setResource(binary);
+		
+		Bundle.BundleEntryComponent result = ahk.transformBundleEntry(entry, "user-2");
+		
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getResource().getResourceType().name(), "Observation");
+		Observation observation = (Observation) result.getResource();
+		Assertions.assertEquals(observation.getSubject().getReferenceElement().getIdPart(), "user-2");
 	}
 }
