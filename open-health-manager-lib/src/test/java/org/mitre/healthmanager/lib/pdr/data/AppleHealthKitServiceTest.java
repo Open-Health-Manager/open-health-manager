93c0fd3bde0b03e07e7b153fd0f16fea3a505fef
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2022-10-31 13:09:24 -0400

    Add test that ensures the convert pregnancy works correctly and creates 2 observation resources when a valid end date is given and 1 when it is not

--- open-health-manager-lib/src/test/java/org/mitre/healthmanager/lib/pdr/data/AppleHealthKitServiceTest.java
+++ open-health-manager-lib/src/test/java/org/mitre/healthmanager/lib/pdr/data/AppleHealthKitServiceTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.hl7.fhir.r4.model.Binary;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Condition;
+import org.hl7.fhir.r4.model.Observation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -35,5 +36,40 @@ class AppleHealthKitServiceTest {
 			Assertions.assertEquals(condition.getSubject().getReferenceElement().getIdPart(), "user-2");
 			Assertions.assertNull(condition.getAsserter().getReferenceElement().getValue());
 		}
+	}
+
+	@Test
+	void testConvertPregnancy() throws IOException {
+
+		String pregnancyMessage = "{\"uuid\":\"24827DF1-BB01-4D39-A3C5-3F29A4C8FC5B\",\"value\":\"notApplicable\",\"startDate\":\"2022-09-03\",\"endDate\":\"2023-06-03\",\"sampleType\":\"HKCategoryTypeIdentifierPregnancy\"}";
+
+		Binary pregnancyResource = new Binary();
+		pregnancyResource.setContentType("application/json");
+		pregnancyResource.setContentAsBase64(Base64.getEncoder().encodeToString(pregnancyMessage.getBytes()));
+		
+		Bundle.BundleEntryComponent entry = new Bundle.BundleEntryComponent();
+		entry.setResource(pregnancyResource);
+		
+		List<Bundle.BundleEntryComponent> resultList = ahk.transformBundleEntry(entry, "user-2");
+		Assertions.assertEquals(2, resultList.size());
+		for (Bundle.BundleEntryComponent result : resultList) {
+			Assertions.assertNotNull(result);
+			Assertions.assertEquals("Observation", result.getResource().getResourceType().name());
+			Observation observation = (Observation) result.getResource();
+			Assertions.assertEquals(observation.getSubject().getReferenceElement().getIdPart(), "user-2");
+		}
+
+		pregnancyMessage= "{\"uuid\":\"34827DF1-BB01-4D39-A3C5-3F29A4C8FC5B\",\"value\":\"notApplicable\",\"startDate\":\"2022-09-03\",\"endDate\":\"4000-12-31\",\"sampleType\":\"HKCategoryTypeIdentifierPregnancy\"}";
+		pregnancyResource.setContentAsBase64(Base64.getEncoder().encodeToString(pregnancyMessage.getBytes()));
+
+		resultList = ahk.transformBundleEntry(entry, "user-2");
+		Assertions.assertEquals(1, resultList.size());
+		for (Bundle.BundleEntryComponent result : resultList) {
+			Assertions.assertNotNull(result);
+			Assertions.assertEquals("Observation", result.getResource().getResourceType().name());
+			Observation observation = (Observation) result.getResource();
+			Assertions.assertEquals(observation.getSubject().getReferenceElement().getIdPart(), "user-2");
+		}
+
 	}
 }
